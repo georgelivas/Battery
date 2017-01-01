d345b9dd62221cdcd0ec3ce4da83bc96fc85d3b6
George Livas <giorgos.livas.99@gmail.com>
2017-01-01 23:10:18 +0200

    Initial Commit

---
+++ Battery/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Battery
+//
+//  Created by Giorgos livas on 01/01/2017.
+//  Copyright © 2017 Giorgos livas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
